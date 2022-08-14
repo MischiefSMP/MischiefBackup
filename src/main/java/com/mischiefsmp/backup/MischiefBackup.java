1f52ebfb4ee39888a1af5c16e12c6ad66a8a8448
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-08-14 11:02:33 +0200

    Create MischiefBackup.java

---
+++ src/main/java/com/mischiefsmp/backup/MischiefBackup.java
@@ -0,0 +1,26 @@
+package com.mischiefsmp.backup;
+
+import com.mischiefsmp.core.api.MischiefPlugin;
+
+public class MischiefBackup extends MischiefPlugin {
+
+    @Override
+    public void init1_onLoad() {
+
+    }
+
+    @Override
+    public void init2_onEnable() {
+
+    }
+
+    @Override
+    public void init3_onDone() {
+
+    }
+
+    @Override
+    public void init4_onDisable() {
+
+    }
+}
